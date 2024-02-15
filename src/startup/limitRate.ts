b2bfdf975612678409f1cd200d5e7b8a51f54576
Erik <erik.p.schmitt@gmail.com>
2024-02-15 22:16:12 +0100

    Trying to fix trust proxy errors.

--- src/startup/limitRate.ts
+++ src/startup/limitRate.ts
@@ -9,7 +9,7 @@ const addRateLimiter = async (app: Express) => {
     const limiter = RateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // max 100 requests per windowMs
-        trustProxy: true, // Enable trust proxy to correctly identify client IP when behind a reverse proxy
+        trustProxy: false, // Enable trust proxy to correctly identify client IP when behind a reverse proxy
     } as ExtendedOptions);
 
     app.use(limiter);
