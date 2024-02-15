6df9173c4795240e2ffaa0319b00216dbf0906aa
Erik <erik.p.schmitt@gmail.com>
2024-02-15 22:09:57 +0100

    Update rate limit because of trsutProxy.

--- src/startup/limitRate.ts
+++ src/startup/limitRate.ts
@@ -1,12 +1,18 @@
 import { Express } from 'express';
-import RateLimit from 'express-rate-limit';
+import RateLimit, { Options } from 'express-rate-limit';
+
+interface ExtendedOptions extends Options {
+    trustProxy?: boolean;
+}
 
 const addRateLimiter = async (app: Express) => {
     const limiter = RateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // max 100 requests per windowMs
-    });
+        trustProxy: true, // Enable trust proxy to correctly identify client IP when behind a reverse proxy
+    } as ExtendedOptions);
 
     app.use(limiter);
 };
+
 export default addRateLimiter;
