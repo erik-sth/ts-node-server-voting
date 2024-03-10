096189c133ad2f7f24adfd1ed4077135dafa0688
Erik <erik.p.schmitt@gmail.com>
2024-03-10 22:01:13 +0100

    Fully implemented baseAccess middleware.

--- src/middleware/baseAccess.ts
+++ src/middleware/baseAccess.ts
@@ -1,22 +1,42 @@
-import { Request, Response, NextFunction } from 'express';
-import { isValidObjectId } from 'mongoose';
+import { Response, NextFunction } from 'express';
+import mongoose from 'mongoose'; // Import mongoose
 import logger from '../utils/logger';
+import { User } from '../models/user';
+import { AuthenticatedRequest } from '../types/Request.types';
 
-const baseAccess = (req: Request, res: Response, next: NextFunction) => {
+const baseAccess = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+) => {
     try {
         if (
             req.params &&
             req.params.projectId &&
-            isValidObjectId(req.params.projectId)
+            mongoose.isValidObjectId(req.params.projectId) // Use mongoose.isValidObjectId
         ) {
-            next();
+            const projectId: mongoose.Schema.Types.ObjectId =
+                new mongoose.Schema.Types.ObjectId(req.params.projectId); // Use mongoose.Schema.Types.ObjectId
+            const user = req.user; // Simplify the user retrieval
+            if (user && user._id) {
+                const foundUser = await User.findById(user._id);
+                if (foundUser && foundUser.projects.includes(projectId)) {
+                    next();
+                } else {
+                    throw new Error('No access to this project');
+                }
+            } else {
+                throw new Error(
+                    'User not authenticated or missing _id property'
+                );
+            }
         } else {
-            throw new Error('projectId parameter is missing');
+            throw new Error('projectId parameter is missing or invalid');
         }
     } catch (error) {
-        logger.debug('Invalid ObjectId');
-        res.send('Invalid objectId.').status(400);
+        logger.debug(`Invalid ObjectId: ${error.message}`);
+        res.status(400).send('Invalid objectId or insufficient access.');
     }
 };
 
-export { baseAccess };
+export { baseAccess, AuthenticatedRequest };
