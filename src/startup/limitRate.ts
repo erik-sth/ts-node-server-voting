24a09117bcaa983cca27c4c09110d530e39fa9b3
Erik <erik.p.schmitt@gmail.com>
2024-05-03 01:20:55 +0200

    Added headers and trust proxy.

--- src/startup/limitRate.ts
+++ src/startup/limitRate.ts
@@ -9,6 +9,8 @@ const addRateLimiter = async (app: Express) => {
     const limiter = RateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // max 100 requests per windowMs
+        headers: true,
+        trustProxy: true,
     } as ExtendedOptions);
 
     app.use(limiter);
